7fc6ecfe6631a050b64131f05194618814bb5b92
BruderRaziel <nico.aldag@web.de>
2023-07-13 16:51:42 +0200

    Alles auskommentiert

--- script_01.js
+++ script_01.js
@@ -53,18 +53,18 @@
 
 /***** 04 Beispiel - Berechnung ******/
 
-// Deklaration
-let ageJohn, ageMark;
-const birthYearJohn = 2000;
-const birthYearMark = 1990;
+// // Deklaration
+// let ageJohn, ageMark;
+// const birthYearJohn = 2000;
+// const birthYearMark = 1990;
 
-// Berechnung
-let date = new Date();  // Systemfunktion Datum
-let year = date.getFullYear(); // spezifische Daten liefern lassen aus Variable -date-
-ageJohn = year - birthYearJohn;
-ageMark = year - birthYearMark;
+// // Berechnung
+// let date = new Date();  // Systemfunktion Datum
+// let year = date.getFullYear(); // spezifische Daten liefern lassen aus Variable -date-
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
 
-// Ausgabe
-console.log("ageJohn; " + ageJohn)
-console.log("ageMark: " + ageMark)
+// // Ausgabe
+// console.log("ageJohn; " + ageJohn)
+// console.log("ageMark: " + ageMark)
 
