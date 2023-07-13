f30aca0fb94c1bc86970cdcfbf1fe5f0437ded9a
Nico <nico.aldag@web.de>
2023-07-13 11:46:01 +0200

    03a) Variablen und Konstanten

--- script_01.js
+++ script_01.js
@@ -15,10 +15,30 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName;  // Deklaration (Definition)
-firstName = "Max";  // Werzuweisung
-console.log(firstName)  // Ausgabe
+// let firstName;  // Deklaration (Definition)
+// firstName = "Max";  // Werzuweisung
+// console.log(firstName)  // Ausgabe
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 02 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben: ");
+// familyName = prompt("Bitte Nachnamen eingeben: ");
+
+// console.log(firstName + " " + familyName);
+
+//JS ist eine untypisierte Sprache!
+// let test;
+// test = "Maxine";
+// test = 2;
+// test = false;
+
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+
